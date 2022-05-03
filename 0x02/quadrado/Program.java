672d75eccb21fb1952b32d3ea8fd31dea17fcdbc
Hamaraisa <hamaraisa@ciandt.com>
2022-05-03 14:39:36 -0300

    Ola

---
+++ 0x02/quadrado/Program.java
@@ -0,0 +1,24 @@
+package quadrado;
+
+public class Program {
+    public static void main(String[] args) {
+            Quadrado quadrado = new Quadrado();
+
+        try {
+            double lado1 = -5;
+            double resultado = Quadrado.area(lado1);
+            System.out.println(resultado);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        try{
+            double lado2 = 3;
+            double resultado = Quadrado.area(lado2);
+            System.out.println(resultado);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+
+        }
+    }
+}
+
---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,11 @@
+package quadrado;
+
+public class Quadrado {
+    public static double area(double lado1) {
+        if (lado1 < 0) {
+            IllegalArgumentException erro = new IllegalArgumentException("Lado deve possuir valor positivo");
+            throw erro;
+        }
+        return lado1*lado1;
+    }
+}

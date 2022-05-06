32b245a8e5837db50b8a57638503c64e3eb1a71f
Hamaraisa <hamaraisa@ciandt.com>
2022-05-06 12:13:04 -0300

    Classes e Objetos

---
+++ 0x05/heranca/Program.java
@@ -0,0 +1,16 @@
+package heranca;
+
+public class Program {
+    public static void main(String[] args) {
+        Animal meuAnimal = new Animal();
+        Cachorro meuCachorro = new Cachorro();
+        Object obj = meuCachorro;
+        Animal umOutroAnimal = meuCachorro;
+
+        System.out.println("meuAnimal eh um " + meuAnimal.getClass().getSimpleName());
+        System.out.println("meuCachorro eh um " + meuCachorro.getClass().getSimpleName());
+        System.out.println("objeto eh um " + obj.getClass().getSimpleName());
+        System.out.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getSimpleName());
+    }
+}
+

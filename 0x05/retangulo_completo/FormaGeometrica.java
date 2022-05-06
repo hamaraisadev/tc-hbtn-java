e6cebb2079e0caf3232a2597284df761161716d3
Hamaraisa <hamaraisa@ciandt.com>
2022-05-06 11:10:31 -0300

    Classes e Objetos

---
+++ 0x05/retangulo_completo/FormaGeometrica.java
@@ -0,0 +1,10 @@
+package retangulo_completo;
+
+public class FormaGeometrica {
+
+    public double area() {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
+
+

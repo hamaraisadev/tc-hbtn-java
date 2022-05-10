7c6e96a44f1c2abcd403345ad9f27c134ef27f26
Hamaraisa <hamaraisa@ciandt.com>
2022-05-10 16:28:23 -0300

    Associação entre Classes

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,49 @@
+public class Empregado {
+
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    public Empregado(String nome, int codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void apresentar(){
+        System.out.println("Codigo: " + getCodigo());
+        System.out.println("Nome: " + getNome());
+        System.out.println("Bairro: " + this.getEndereco().getBairro());
+        System.out.println("Cidade: " + this.getEndereco().getCidade());
+        System.out.println("Pais: " + this.getEndereco().getPais());
+    }
+}

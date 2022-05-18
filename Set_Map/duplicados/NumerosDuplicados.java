508b55b41dfba7aacbad6ab49db08eb7e1e2ff71
Hamaraisa <hamaraisa@ciandt.com>
2022-05-18 11:14:20 -0300

    Duplicados

---
+++ Set_Map/duplicados/NumerosDuplicados.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class NumerosDuplicados {
+    public static TreeSet<Integer> buscar(int[] numeros){
+        List<Integer> list = Arrays.stream(numeros).boxed().collect(Collectors.toList());
+
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        List<Integer> filtrarDuplicados = list.stream()
+                .filter(i -> !hashSet.add(i))
+                .collect(Collectors.toList());
+
+        return new TreeSet<>(filtrarDuplicados);
+    }
+}
+

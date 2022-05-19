c45bb1cca510fd2fd54e35dc8a0db001361e0a91
Hamaraisa <hamaraisa@ciandt.com>
2022-05-19 10:37:43 -0300

    Java – Collections (Exercícios de fixação)

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Blog {
+
+    private List<Post> posts = new ArrayList<>();
+
+    public void adicionarPostagem(Post post) {
+        posts.add(post);
+
+    }
+
+    public Set<String> obterTodosAutores(){
+        Set<String> autores = new TreeSet<>();
+        for (Post post : posts){
+            autores.add(post.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria() {
+        Map<String, Integer> contagem = new TreeMap<>();
+        for (Post post : posts) {
+            if (contagem.containsKey(post.getCategoria())) {
+                contagem.put(post.getCategoria(),contagem.get(post.getCategoria())+1);
+            }else {
+                contagem.put(post.getCategoria(), 1);
+            }
+        }
+        return contagem;
+    }
+}
+

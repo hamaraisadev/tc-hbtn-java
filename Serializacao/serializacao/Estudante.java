02b413794840c008a9a588292c080156b841d6b7
Hamaraisa <hamaraisa@ciandt.com>
2022-06-02 13:17:39 -0300

    Serialização

---
+++ Serializacao/serializacao/Estudante.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class Estudante implements Serializable {
+
+    private int idade;
+    private String nome;
+    transient private String senha;
+
+    public Estudante(int idade, String nome, String senha) {
+        this.idade = idade;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String toString() {
+        return "Estudante { nome='" + nome + "', idade='" + idade + "', senha='" + senha + "' }";
+    }
+
+
+
+
+}
